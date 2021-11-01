64c8db380240485c3f12571a94bff8a0c29b2e71
Nagah <E-n.Shinawy@lean.sa>
2021-11-01 07:17:50 +0300

    inputs

---
+++ masterclass/4-inputs.py
@@ -0,0 +1,8 @@
+
+name = input("Please enter your name : ")
+job = input("Please enter your job")
+location = input("Please enter your location")
+
+info = f"Your name is '{name}'.\nYour job is '{job}'.\nYour location is '{location}'."
+
+print(info)
