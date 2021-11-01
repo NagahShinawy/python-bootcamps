97be1b3b3b2265a0e6dc2afb48d2e86b686bbb99
Nagah <E-n.Shinawy@lean.sa>
2021-11-01 07:47:25 +0300

    vars

---
+++ masterclass/vars.py
@@ -0,0 +1,23 @@
+# something to hold data
+
+name = "John"
+
+image = "profile.png"
+
+print(name)
+
+print(image)
+
+location = "Cairo"
+
+print(f"{name} is at {location}")
+
+# print(job_title)  # NameError: name 'job_title' is not defined
+
+salary = 23.65
+
+print(salary)
+
+del salary
+
+# print(salary)  # NameError: name 'salary' is not defined
