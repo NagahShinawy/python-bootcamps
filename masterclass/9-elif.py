deb8bff7d36d242abd8fb1ae933824bb98ed5a3f
Nagah <E-n.Shinawy@lean.sa>
2021-11-01 08:27:40 +0300

    elif

---
+++ masterclass/9-elif.py
@@ -0,0 +1,27 @@
+
+grade = int(input("Please enter your grade: "))
+
+if grade in range(0, 50):
+    print("Failed")
+
+elif grade in range(50, 66):
+    print("Passed")
+
+elif grade in range(66, 76):
+    print("Good")
+
+
+elif grade in range(76, 86):
+    print("Very Good")
+
+
+elif grade in range(86, 101):
+    print("Excellent")
+
+
+else:
+    print("Invalid Grade")
+
+
+
+
