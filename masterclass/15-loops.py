86555a01d8e42a0574f50e115dfcfb080c3e5463
Nagah <E-n.Shinawy@lean.sa>
2021-11-02 08:53:38 +0300

    loops

---
+++ masterclass/15-loops.py
@@ -0,0 +1,21 @@
+
+for i in range(1, 11):
+    print(i, end=" ")
+
+
+print("")
+
+
+def send_email(recipient):
+    print(f"sending to '{recipient}'")
+
+
+emails = [
+    "john@test.com",
+    "james@test.com",
+    "adam@test.com",
+    "loen@test.com"
+]
+
+for email in emails:
+    send_email(email)
