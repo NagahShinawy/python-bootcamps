ffd9acdf9af0d64111d8b27eef9f4af27240ec45
Nagah <E-n.Shinawy@lean.sa>
2021-11-03 04:26:24 +0300

    modules

---
+++ masterclass/21-modules.py
@@ -0,0 +1,14 @@
+from random import choice
+
+names = [
+    "john",
+    "adam",
+    "james",
+    "anna",
+    "sara",
+    "angela",
+    "loen"
+]
+
+for _ in range(3):
+    print(choice(names))
