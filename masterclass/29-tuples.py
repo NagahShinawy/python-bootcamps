f40bedf6a2a2d00c4180e1d848979170095d5cdf
Nagah <E-n.Shinawy@lean.sa>
2021-11-04 09:49:38 +0300

    tuples

---
+++ masterclass/29-tuples.py
@@ -0,0 +1,28 @@
+DAYS = ("Sat", "Sun", "Mon", "Tue", "Wen", "Thr", "Friday")
+
+print(DAYS)
+#               0      1     2    3     4       5     6     7
+BLOOD_TYPES = ("A+", "A-", "O+", "B-", "AB+", "AB-", "O-", "B+")
+
+print(BLOOD_TYPES[0])
+print(BLOOD_TYPES[1])
+print(BLOOD_TYPES[2 + 1])
+
+
+print("#" * 100)
+
+# negative index
+print(BLOOD_TYPES[-1])
+print(BLOOD_TYPES[-2])
+
+
+print("#" * 100)
+
+STATUS = ("Pending", "Approve", "Reject")
+
+
+print(STATUS)
+
+GENDER = ("Male", "Female")
+
+print(GENDER)
