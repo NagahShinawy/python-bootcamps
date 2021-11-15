10cf94960ea420c5fde8b75f5405a46528d9463d
Nagah <E-n.Shinawy@lean.sa>
2021-11-15 06:56:26 +0300

    oop with interface

--- masterclass/43-oop.py
+++ masterclass/43-oop.py
@@ -1,3 +1,7 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+
 class BaseError(Exception):
     _message = "error occurred"
 
@@ -33,7 +37,75 @@ class PhoneNumberInvalid(BaseError):
     _message = "Enter a valid phone number"
 
 
-python = Language(name="python", users=100, level="high")
+class JsonMixin(ABC):
 
-if 1:
-    raise PhoneNumberInvalid
+    @abstractmethod
+    def to_json(self):
+        pass
+
+
+class Seller(JsonMixin):
+
+    def __init__(self, name):
+        self.name = name
+
+    def to_json(self):
+        return {
+            "name": self.name
+        }
+
+    def __str__(self):
+        return self.name
+
+
+class Product(JsonMixin):
+
+    def __init__(self, name: str, price: float, seller: Seller):
+        self.name = name
+        self.price = price
+        self.seller = seller
+
+    def __str__(self):
+        return f"{self.name} - {self.price}"
+
+    def to_json(self):
+        return {
+            "name": self.name,
+            "price": self.price,
+            "seller": self.seller.to_json()
+        }
+
+
+class Order(JsonMixin):
+
+    def __init__(self, products: List[Product]):
+        self.products = products
+
+    def __str__(self):
+        return f"Order({len(self.products)})"
+
+    def __len__(self):
+        return self.products
+
+    def to_json(self):
+        return [product.to_json() for product in self.products]
+
+
+def main():
+    python = Language(name="Python", users=100, level="high")
+    # ############# ############# ############# ############# ############# ############
+    amazon = Seller("Amazon")
+    noon = Seller("noon")
+    extra = Seller("Extra")
+
+    phone = Product("iphone", 1000, noon)
+    laptop = Product("macbook pro", 2000, amazon)
+    screen = Product("dell", 500, extra)
+
+    order = Order(products=[phone, laptop, screen])
+    print(order)
+    print(order.to_json())
+
+
+if __name__ == "__main__":
+    main()
