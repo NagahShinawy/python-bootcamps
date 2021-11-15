7a14d7f72f3f6669465c3262abe986b3219c4ba8
Nagah <E-n.Shinawy@lean.sa>
2021-11-15 15:02:42 +0300

    list, tuples

--- javatpoint/3-data-types.py
+++ javatpoint/3-data-types.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 userid = 12  # int
 name = "Loen"  # str
 salary = 1000.4  # float
@@ -22,3 +24,25 @@ x = 1 + 3j
 print(f"X = {x} is {type(x)} data type")
 
 print(isinstance(x, complex))  # True
+
+
+# lists
+
+items = [4, 5.6, "test", False]
+
+print(items * 2)  # [4, 5.6, 'test', False, 4, 5.6, 'test', False]
+
+
+def clean(items: Union[list, tuple]):
+    return [item for item in items if item]
+
+
+print(clean(items))
+
+
+# tuple:  is a read-only data structure as we can't modify the size and value of the items of a tuple
+
+
+# concatenate tuples
+
+print(("red", "green") + ("blue", "white"))  # ('red', 'green', 'blue', 'white')
