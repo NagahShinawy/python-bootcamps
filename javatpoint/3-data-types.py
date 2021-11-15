9a74f4550559dc1cea05e4d19c04fde0179ea8c5
Nagah <E-n.Shinawy@lean.sa>
2021-11-15 13:43:44 +0300

    numbers data types

---
+++ javatpoint/3-data-types.py
@@ -0,0 +1,24 @@
+userid = 12  # int
+name = "Loen"  # str
+salary = 1000.4  # float
+is_male = True  # boolean
+
+# ################ ################ ################ ################ ################ ###############
+print(userid, type(userid))
+print(name, type(name))
+print(salary, type(salary))
+print(is_male, type(is_male))
+
+
+# ################ ################ ################ ################ ################ ###############
+
+# Standard data types: LOOK IMAGE
+
+# numbers:
+
+# int, float, complex
+
+x = 1 + 3j
+print(f"X = {x} is {type(x)} data type")
+
+print(isinstance(x, complex))  # True
