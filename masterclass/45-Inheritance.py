ae6d7c44f771df732e63c1dbc16b397a6d52a601
Nagah <E-n.Shinawy@lean.sa>
2021-11-17 07:12:26 +0300

    protected, Mixin for crud operations, *args real example

---
+++ masterclass/45-Inheritance.py
@@ -0,0 +1,108 @@
+from abc import ABC
+
+
+# using protected attributes
+
+# Mixin for crud operations
+
+# *args real example ==> can create one or many courses
+
+
+class CRUDMixin:
+    def __init__(self):
+        self.objects = []
+
+    def _create(self, obj):
+        if obj not in self.objects:
+            self.objects.append(obj)
+
+    def _delete(self, obj):
+        if obj in self.objects:
+            self.objects.remove(obj)
+
+    def _create_all(self, *args):
+        for obj in args:
+            self._create(obj)
+
+    def _delete_all(self, *args):
+        for obj in args:
+            self._delete(obj)
+
+    @property
+    def count(self):
+        return len(self.objects)
+
+    def __len__(self):
+        return len(self.objects)
+
+
+class Course:
+    def __init__(self, title, duration):
+        self.title = title
+        self.duration = duration
+
+    def __repr__(self):
+        return f"<{self.title} - {self.duration}>"
+
+
+class Member(ABC, CRUDMixin):
+    def __init__(
+        self, name: str, dob: str, phone: str, email: str, address=None, *args, **kwargs
+    ):
+        super().__init__()
+        self.name = name
+        self.dob = dob
+        self.phone = phone
+        self.email = email
+        self.address = address
+        self.courses = self.objects
+
+    def __str__(self):
+        return f"{self.__class__.__name__}<{self.name} - {self.phone}>"
+
+    def join_course(self, course: Course):
+        self._create(course)
+
+    def join_courses(self, *args: Course):
+        self._create_all(*args)
+
+    def remove_course(self, course: Course):
+        self._delete(course)
+
+
+class Student(Member):
+    def __init__(self, class_, name, dob, phone, email, address=None, *args, **kwargs):
+        self.marks = []
+        self.class_ = class_
+        super().__init__(name, dob, phone, email, address=address, *args, **kwargs)
+
+
+class Teacher(Member):
+    pass
+
+
+def main():
+    django = Course("Django", "10H")
+    gui = Course("PyQt GUI", "5H")
+    flask = Course("Flask Micro Framework", "3H")
+    nodejs = Course("NodeJs", "7H")
+    php = Course("PHP", "4H")
+
+    john = Student(class_="A", name="John", dob="01-01-1999", phone="+20127444444", email="jonh@test.con")
+    leon = Student(class_="B", name="Loen", dob="01-01-2000", phone="+20127555555", email="loen@test.con")
+
+    print(john)
+    john.join_course(flask)
+    john.join_course(django)
+    print(john.courses)
+
+    print(john.count)
+    print(len(john))
+
+    print("#" * 100)
+    leon.join_courses(php, nodejs, gui)
+    print(leon.courses)
+
+
+if __name__ == '__main__':
+    main()
