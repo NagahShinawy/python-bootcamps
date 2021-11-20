64fb92928a3aeb26274936af5c3edd73129187a7
Nagah <E-n.Shinawy@lean.sa>
2021-11-20 11:06:08 +0300

    get today date with 2 ways

---
+++ 100days/pythontalk/days/day1-datetime.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+from datetime import date
+
+# get current today date
+
+# 1- using datetime
+today = datetime.today()
+
+print(today)  # 2021-11-20 11:04:41.458327
+print(
+    type(today)  # <class 'datetime.datetime'>
+)  # TypeError: descriptor 'date' of 'datetime.datetime' object needs an argument
+# print(datetime.date())
+
+
+print(today.date())  # 2021-11-20
+
+
+# 2- using date
+print(date.today())  # 2021-11-20
