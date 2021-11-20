e30a70fb2ab8f6e45508f5709d196cba007b7d2a
Nagah <E-n.Shinawy@lean.sa>
2021-11-20 08:39:19 +0300

    mapping days

--- 100days/pythontalk/1-course_flow.py
+++ 100days/pythontalk/1-course_flow.py
@@ -2,6 +2,10 @@
 # list of tuples
 # slicing
 # skip by 3
+# dictionary comprehensive
+# tuple unpacking
+# update method dictionary
+# constants
 
 DAY = "Day"
 
@@ -14,3 +18,17 @@ groups = [(day, day + 3) if day == LAST_END else (day, day + 2) for day in DAYS[
 print(groups)
 
 print(list(DAYS))
+map_days = {f"{DAY}#{group[0]}-{group[1]}": list(range(group[0], group[1] + 1)) for group in groups}
+
+print(map_days)
+
+map_days = dict()
+
+for group in groups:
+    start, end = group
+    duration = f"{DAY}#{start}-{end}"
+    days = list(range(start, end + 1))
+    map_days.update({duration: days})
+
+
+print(map_days)
