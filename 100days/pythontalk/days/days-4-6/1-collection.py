b72ec6613b12e69c3c673dd7636df15269fcc7d3
Nagah <E-n.Shinawy@lean.sa>
2021-11-23 08:10:28 +0300

    namedtuple more examples

--- 100days/pythontalk/days/days-4-6/1-collection.py
+++ 100days/pythontalk/days/days-4-6/1-collection.py
@@ -1,7 +1,7 @@
 from collections import namedtuple
 
 
-# A `namedtuple` is a convenient way to define a class without methods.
+# A `namedtuple` is a convenient (مريح) way to define a class without methods.
 # This allows you to store `dict` like objects you can access by attributes. Let's first look at a classic `tuple`:
 
 users = [
@@ -11,6 +11,7 @@ users = [
     ("adam", "1999-01-01", "adam@test.com"),
 ]
 
+# PROBLEM: The order is not really meaningful leading to ugly code to output the data:
 
 # WITHOUT namedtuple
 print("Username", "Date of birth", "Email")
@@ -21,6 +22,8 @@ for user in users:
 
 # ########################## ############################## ##########################################
 # With namedtuple
+# SOLUTION: using attributes rather than indexing (order)
+
 print("#" * 100)
 
 
@@ -29,3 +32,62 @@ User = namedtuple("User", ["username", "dob", "email"])
 for user in users:
     obj = User(*user)
     print(obj.username, obj.dob, obj.email)  # more readable
+
+# ########################## ############################## ##########################################
+
+print("#" * 100)
+
+Location = namedtuple("Location", "long,lat")
+
+work = Location(23.54, 65.9)
+print(work)  # Location(long=23.54, lat=65.9)
+print(type(work))  # <class '__main__.Location'>
+print(f"Long: {work.long}")
+print(f"Lat: {work.lat}")
+
+
+# ########################## ############################## ##########################################
+
+print("#" * 100)
+
+book = ("clean code", 10, "uncle bob", "2000-01-01")
+
+# not readable code, it is ugly code
+
+print(f"'{book[0]}' by '{book[2]}' is '{book[1]}$' was published at '{book[3]}'")
+
+
+Book = namedtuple("Book", "title price author published")
+
+clean_code = Book("Clean Code", 14, "Bob", "2000-01-01")  # <class '__main__.Book'>
+
+print(
+    clean_code
+)  # Book(title='Clean Code', price=13, author='Bob', published='2000-01-01')
+
+
+print(
+    f"'{clean_code.title}' by '{clean_code.author}' is '{clean_code.price}$' was published at '{clean_code.published}'"
+)
+
+print(clean_code.count(14))  # count comes from tuple class
+
+print(type(clean_code))
+
+
+# namedtuple inherits from tuple
+
+print(Book.mro())  # [<class '__main__.Book'>, <class 'tuple'>, <class 'object'>]
+
+
+# ########################## ############################## ##########################################
+
+print("#" * 100)
+
+clean_arch = Book(title="Clean Arch", published="1999-01-01", price=16, author="James")
+
+print(clean_arch.title, clean_arch.author)
+print(
+    clean_arch
+)  # Book(title='Clean Arch', price=16, author='James', published='1999-01-01')
+print(type(clean_arch))  # <class '__main__.Book'>
