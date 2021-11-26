f968951a69adee8b739690ab19584bd80518efba
Nagah <E-n.Shinawy@lean.sa>
2021-11-26 11:14:48 +0300

    strings

---
+++ websites/javatpoint/12-strings.py
@@ -0,0 +1,19 @@
+#  strings are treated as the sequence of characters
+
+#  which means that Python doesn't support the character data-type; instead,
+#  a single character written as 'p' is treated as the string of length 1.
+
+# strings
+# constants
+
+welcome = "Hello World"
+
+print(welcome, type(welcome))
+
+ACCEPT = "Y"  # string of single letter with length 1
+
+REFUSED = "N"
+
+
+print(ACCEPT)
+print(REFUSED)
