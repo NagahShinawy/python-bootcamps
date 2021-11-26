f3a4235cb05336285acadc3d1c1541b0b1472686
Nagah <E-n.Shinawy@lean.sa>
2021-11-26 10:31:31 +0300

    while example

--- websites/javatpoint/9-while.py
+++ websites/javatpoint/9-while.py
@@ -3,3 +3,32 @@ while expression:
     statements
 """
 
+# while [pre tested loop]
+
+# The Python while loop allows a part of the code to be executed until the given condition returns false.
+# It is also known as a pre-tested loop
+
+# The expression should be any valid Python expression resulting in true or false
+
+
+# # prints all letters except 'a' and 't'
+
+website = "javatpoint"
+EXCLUDED_LETTERS = "at"
+
+print(website)
+
+for letter in website:
+    if letter in EXCLUDED_LETTERS:
+        continue
+    print(letter, end="")
+
+
+print()
+# stop when find letter 'p'
+
+for letter in website:
+    if letter == 'p':
+        break
+
+    print(letter, end="")
